187447469dab8b12deb1a079339d03433e920ea3
Frank Hampus Weslien <frankhampusweslien@gmail.com>
2025-09-18 08:17:31 +0200

    feat: add html DSL

---
+++ lib/html.ex
@@ -0,0 +1,121 @@
+defmodule MetaProgramming.Html do
+  @external_resource tags_path = Path.join([__DIR__, "../priv/tags.txt"])
+  @tags (for line <- File.stream!(tags_path, [], :line) do
+           line |> String.trim() |> String.to_atom()
+         end)
+
+  for tag <- @tags do
+    defmacro unquote(tag)(attrs, do: inner) do
+      tag = unquote(tag)
+      quote do: tag(unquote(tag), unquote(attrs), do: unquote(inner))
+    end
+
+    defmacro unquote(tag)(attrs \\ []) do
+      tag = unquote(tag)
+      quote do: tag(unquote(tag), unquote(attrs))
+    end
+  end
+
+  defmacro markup(do: block) do
+    quote do
+      import Kernel, except: [div: 2]
+      {:ok, var!(buffer, MetaProgramming.Html)} = start_buffer([])
+      unquote(block)
+      result = render(var!(buffer, MetaProgramming.Html))
+      :ok = stop_buffer(var!(buffer, MetaProgramming.Html))
+      result
+    end
+  end
+
+  def start_buffer(state), do: Agent.start_link(fn -> state end)
+
+  def stop_buffer(buffer), do: Agent.stop(buffer)
+
+  def put_buffer(buffer, content), do: Agent.update(buffer, fn state -> [content | state] end)
+
+  def render(buffer), do: Agent.get(buffer, & &1) |> Enum.reverse() |> Enum.join("")
+
+  defmacro tag(name, attrs \\ []) do
+    {inner, attrs} = Keyword.pop(attrs, :do)
+    quote do: tag(unquote(name), unquote(attrs), do: unquote(inner))
+  end
+
+  defmacro tag(name, attrs, do: inner) do
+    quote do
+      put_buffer(var!(buffer, MetaProgramming.Html), open_tag(unquote_splicing([name, attrs])))
+      unquote(inner)
+      put_buffer(var!(buffer, MetaProgramming.Html), "</#{unquote(name)}>")
+    end
+  end
+
+  def open_tag(name, []) do
+    "<#{name}>"
+  end
+
+  def open_tag(name, attrs) do
+    attr_html = for {key, val} <- attrs, into: "", do: "#{key}=\"#{val}\""
+    "<#{name} #{attr_html}>"
+  end
+
+  defmacro text(content) do
+    quote do
+      put_buffer(var!(buffer, MetaProgramming.Html), to_string(unquote(content)))
+    end
+  end
+end
+
+defmodule MetaProgramming.Template do
+  import MetaProgramming.Html
+
+  def render1 do
+    markup do
+      tag :table do
+        tag :tr do
+          for i <- 0..5 do
+            tag :td do
+              text("Cell #{i}")
+            end
+          end
+        end
+      end
+
+      tag :div do
+        text("Some Nested Content")
+      end
+    end
+  end
+
+  def render2 do
+    markup do
+      table do
+        tr do
+          for i <- 0..5 do
+            td do
+              text("Cell #{i}")
+            end
+          end
+        end
+      end
+
+      div do
+        text("Some Nested Content")
+      end
+    end
+  end
+
+  def render3 do
+    markup do
+      div id: "main" do
+        h1 class: "title" do
+          text("Welcome!")
+        end
+      end
+
+      div class: "row" do
+        div do
+          p(do: text("Hello!"))
+        end
+      end
+    end
+  end
+end
--- test/metaprogamming/translator_test.exs
+++ test/metaprogamming/translator_test.exs
@@ -29,6 +29,8 @@ defmodule MetaProgramming.TranslatorTest do
     )
   end
 
+  # Integration tests
+
   test "it recursively walks translations tree" do
     assert I18n.t("en", "users.title") == "Users"
     assert I18n.t("en", "users.profile.title") == "Profiles"
@@ -59,5 +61,43 @@ defmodule MetaProgramming.TranslatorTest do
   test "converts interpolation values to string" do
     assert I18n.t("fr", "flash.notice.hello", first: 123, last: 456) ==
              "salut 123 456!"
+  end
+
+  # Unit tests
+
+  test "compile/1 generates catch-all t/3 functions" do
+    assert MetaProgramming.Translator.compile([]) |> Macro.to_string() ==
+             String.trim(~S"""
+             def t(locale, path, binding \\ [])
+             []
+
+             def t(_locale, _path, _bindings) do
+               {:error, :no_translation}
+             end
+             """)
+  end
+
+  test "compile/1 generates t/3 functions from each locale" do
+    locales = [{"en", [foo: "bar", bar: "%{baz}"]}]
+
+    assert MetaProgramming.Translator.compile(locales) |> Macro.to_string() ==
+             String.trim(~S"""
+             def t(locale, path, binding \\ [])
+
+             [
+               [
+                 def t("en", "foo", bindings) do
+                   "" <> "bar"
+                 end,
+                 def t("en", "bar", bindings) do
+                   (("" <> "") <> to_string(MetaProgramming.Translator.lookup!(bindings, :baz))) <> ""
+                 end
+               ]
+             ]
+
+             def t(_locale, _path, _bindings) do
+               {:error, :no_translation}
+             end
+             """)
   end
 end
